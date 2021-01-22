d5b7e2b6dd64eb38b7b8a8de4cdf652132ceecf4
danielVFS <danielv759153@gmail.com>
2021-01-22 20:23:11 -0300

    feat: User - entity and repository

---
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,26 @@
+import { EntityRepository, Repository } from 'typeorm';
+
+import User from '../entities/User';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async findByName(name: string): Promise<User | undefined> {
+    const user = this.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    const user = this.findOne({
+      where: {
+        email,
+      },
+    });
+
+    return user;
+  }
+}
