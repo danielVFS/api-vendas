9572394c40c8dddfae1b829a0e1199ead8431a5a
danielVFS <danielv759153@gmail.com>
2021-01-23 20:01:05 -0300

    add: dotenv config

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -1,11 +1,13 @@
 import { compare } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
 
 import { UserRepository } from '../typeorm/repositories/UsersRepository';
 
 import User from '../typeorm/entities/User';
 
 import AppError from '@shared/errors/AppError';
+import { Console } from 'console';
 
 interface IRequest {
   email: string;
@@ -28,6 +30,7 @@ class CreateSessionService {
       throw new AppError('Password incorrect', 401);
     }
 
+    console.log(process.env.SECRET);
     return user;
   }
 }
