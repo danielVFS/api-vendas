723e0649d3ad1caee16184ff7c00ce85838b6eb2
danielVFS <danielv759153@gmail.com>
2021-01-23 14:54:33 -0300

    feat: User post route

---
+++ src/modules/users/routes/users.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+import UsersController from '../controllers/UsersController';
+
+const userRoutes = Router();
+
+const usersController = new UsersController();
+
+userRoutes.post('/', usersController.create);
+
+export default userRoutes;
