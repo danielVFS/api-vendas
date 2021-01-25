5ecdd0b69d049f6e5166e244dde98159de6402e9
danielVFS <danielv759153@gmail.com>
2021-01-25 16:53:38 -0300

    feat: avatar update route

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,13 +1,20 @@
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
+import multer from 'multer';
 
 import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
+
+import uploadConfig from '@config/upload';
 
 import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
 
 const userRoutes = Router();
 
 const usersController = new UsersController();
+const usersAvatarController = new UserAvatarController();
+
+const upload = multer(uploadConfig);
 
 userRoutes.get('/', isAuthenticated, usersController.index);
 userRoutes.post(
@@ -20,6 +27,12 @@ userRoutes.post(
     },
   }),
   usersController.create,
+);
+userRoutes.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  usersAvatarController.create,
 );
 
 export default userRoutes;
