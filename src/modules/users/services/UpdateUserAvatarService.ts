f4433678ce3ff256015598f56c14ed0789bc4728
danielVFS <danielv759153@gmail.com>
2021-01-25 16:21:04 -0300

    feat: UpdateUserAvatarService

---
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,46 @@
+import path from 'path';
+import fs from 'fs';
+import { getCustomRepository } from 'typeorm';
+
+import { UserRepository } from '../typeorm/repositories/UsersRepository';
+
+import User from '../typeorm/entities/User';
+
+import AppError from '@shared/errors/AppError';
+
+import uploadConfig from '@config/upload';
+
+interface IRequest {
+  user_id: string;
+  avatarFileName: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFileName }: IRequest): Promise<User> {
+    const userRepository = getCustomRepository(UserRepository);
+
+    const user = await userRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found.');
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFileName;
+
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
