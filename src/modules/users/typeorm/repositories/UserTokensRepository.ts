c44ba0fa526dc16baea8717ea785126393957107
danielVFS <danielv759153@gmail.com>
2021-01-26 08:55:42 -0300

    feat: UserTokensRepository

---
+++ src/modules/users/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,26 @@
+import { EntityRepository, Repository } from 'typeorm';
+
+import UserToken from '../entities/UserToken';
+
+@EntityRepository(UserToken)
+export class UserRepository extends Repository<UserToken> {
+  async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.findOne({
+      where: {
+        token,
+      },
+    });
+
+    return userToken;
+  }
+
+  async generate(user_id: string): Promise<UserToken | undefined> {
+    const userToken = this.create({
+      user_id,
+    });
+
+    await this.save(userToken);
+
+    return userToken;
+  }
+}
