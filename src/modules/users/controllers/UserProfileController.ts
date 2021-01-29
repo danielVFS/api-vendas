84347159d0d1157122ded99c0dd853453e432f07
danielVFS <danielv759153@gmail.com>
2021-01-29 09:49:01 -0300

    feat: UserProfileController

---
+++ src/modules/users/controllers/UserProfileController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+
+import ShowProfileService from '../services/ShowProfileService';
+import UpdateProfileService from '../services/UpdateProfileService';
+
+export default class UsersController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const listUser = new ShowProfileService();
+    const user_id = request.user.id;
+
+    const user = await listUser.execute({ user_id });
+
+    return response.json(user);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const updateProfile = new UpdateProfileService();
+
+    const user_id = request.user.id;
+    const { name, email, password, old_password } = request.body;
+
+    const user = await updateProfile.execute({
+      user_id,
+      name,
+      email,
+      password,
+      old_password,
+    });
+
+    return response.json(user);
+  }
+}
