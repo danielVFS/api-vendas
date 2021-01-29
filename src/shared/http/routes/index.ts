e856189be2b3ff352da708a38b64deffdec814ee
danielVFS <danielv759153@gmail.com>
2021-01-29 10:21:19 -0300

    feat: profile routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -4,6 +4,7 @@ import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import sessionsRoutes from '@modules/users/routes/sessions.routes';
 import passwordRoutes from '@modules/users/routes/password.routes';
+import profileRoutes from '@modules/users/routes/profile.routes';
 
 const routes = Router();
 
@@ -11,5 +12,6 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRoutes);
 routes.use('/password', passwordRoutes);
+routes.use('/profile', profileRoutes);
 
 export default routes;
