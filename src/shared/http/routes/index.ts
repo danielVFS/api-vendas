6f59c0d412e3eb4e237330986139ef6b2101f695
danielVFS <danielv759153@gmail.com>
2021-02-02 19:10:30 -0300

    feat: order routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -6,6 +6,7 @@ import sessionsRoutes from '@modules/users/routes/sessions.routes';
 import passwordRoutes from '@modules/users/routes/password.routes';
 import profileRoutes from '@modules/users/routes/profile.routes';
 import costumerRoutes from '@modules/costumers/routes/costumers.routes';
+import ordersRoutes from '@modules/orders/routes/orders.routes';
 
 const routes = Router();
 
@@ -15,5 +16,6 @@ routes.use('/sessions', sessionsRoutes);
 routes.use('/password', passwordRoutes);
 routes.use('/profile', profileRoutes);
 routes.use('/costumers', costumerRoutes);
+routes.use('/orders', ordersRoutes);
 
 export default routes;
