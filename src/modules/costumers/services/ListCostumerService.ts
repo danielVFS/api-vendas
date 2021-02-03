506ee5318fa97c70ed84defd4583d307fb5b4369
danielVFS <danielv759153@gmail.com>
2021-02-03 09:51:17 -0300

    refactor: add pagination

--- src/modules/costumers/services/ListCostumerService.ts
+++ src/modules/costumers/services/ListCostumerService.ts
@@ -4,13 +4,25 @@ import { CostumersRepository } from '../typeorm/repositories/CostumersRepository
 
 import Costumer from '../typeorm/entities/Costumer';
 
+interface IPaginateCostumer {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number;
+  next_page: number;
+  data: Costumer[];
+}
+
 class ListCostumerService {
-  public async execute(): Promise<Costumer[]> {
+  public async execute(): Promise<IPaginateCostumer> {
     const costumerRepository = getCustomRepository(CostumersRepository);
 
-    const costumers = await costumerRepository.find();
+    //const costumers = await costumerRepository.find();
+    const costumers = await costumerRepository.createQueryBuilder().paginate();
 
-    return costumers;
+    return costumers as IPaginateCostumer;
   }
 }
 
