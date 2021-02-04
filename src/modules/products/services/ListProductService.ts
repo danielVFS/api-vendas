406a2741acad906030ed3f5b9dcf4ecd94281352
danielVFS <danielv759153@gmail.com>
2021-02-04 09:54:59 -0300

    feat: RedisCache recover and save service

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -4,11 +4,23 @@ import { ProductRepositoty } from '../typeorm/repositories/ProductsRepository';
 
 import Product from '../typeorm/entities/Product';
 
+import RedisCache from '@shared/cache/RedisCache';
+
 class ListProductService {
   public async execute(): Promise<Product[]> {
     const productsRepository = getCustomRepository(ProductRepositoty);
 
-    const products = await productsRepository.find();
+    const redisCache = new RedisCache();
+
+    let products = await redisCache.recover<Product[]>(
+      'api-vendas-PRODUCT_LIST',
+    );
+
+    if (!products) {
+      products = await productsRepository.find();
+
+      await redisCache.save('api-vendas-PRODUCT_LIST', products);
+    }
 
     return products;
   }
